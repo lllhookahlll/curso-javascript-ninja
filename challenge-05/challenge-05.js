56cc9073f5491a6570421dd0200aaa9f4f06a074
lllhookahlll <58123084+lllhookahlll@users.noreply.github.com>
2020-01-04 01:09:58 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var aleatorio = [400, 'Laranja', true , !true, NaN];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myAleatorio(recebe){
+    return recebe;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myAleatorio(aleatorio)[1]); //'Laranja'
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,7 +22,10 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function myFunction(arr ,nmr){
+    var arr = [nmr];    
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.

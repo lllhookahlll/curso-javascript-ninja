592a7ea1298363369f481b1491b971783c330179
lllhookahlll <58123084+lllhookahlll@users.noreply.github.com>
2020-01-07 09:56:07 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -36,7 +36,11 @@ var varDiferente = ['Mario', function(){}, 5, [1,2], true];
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-varDiferente;
+console.log( myFunction(varDiferente, 0) );
+console.log( myFunction(varDiferente, 1) );
+console.log( myFunction(varDiferente, 2) );
+console.log( myFunction(varDiferente, 3) );
+console.log( myFunction(varDiferente, 4) );
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -54,49 +58,41 @@ os livros.
 */
 function book(nomeLivro){
   var allLivros = {
-    livro1:{quantidadedePaginas: 589, autor: 'Jonatas Gonzaga', editora: 'TiMelo'}, 
-    livro2:{quantidadedePaginas: 258, autor: 'Mario Cesar', editora: 'Moltem'}, 
-    livro3:{quantidadedePaginas: 1459, autor: 'Luiz Carlos', editora: 'Balks'}
+    'A Esperança':{quantidadedePaginas: 589, autor: 'Jonatas Gonzaga', editora: 'TiMelo'}, 
+    'O Fim do Mundo':{quantidadedePaginas: 258, autor: 'Mario Cesar', editora: 'Moltem'}, 
+    'A Revolta':{quantidadedePaginas: 1459, autor: 'Luiz Carlos', editora: 'Balks'}
   };
     
     if (nomeLivro === undefined){
       return allLivros;
     };
-    if (nomeLivro === 'livro1'){
-      return allLivros.livro1;
-    };
-    if (nomeLivro === 'livro2'){
-      return allLivros.livro2;
-    };
-    if (nomeLivro ==='livro3'){
-      return allLivros.livro3;
-    };  
-  
-  
+    
+      return allLivros[ nomeLivro ];  
 }  
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var nomeLivro = 'A Esperança';
+console.log('O autor do livro ' + nomeLivro + ' Tem ' + book('A Esperança').quantidadePaginas + ' páginas' );
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro ' + nomeLivro + ' é ' + book(nomeLivro).autor );
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro ' + nomeLivro + ' foi publicado pela editora ' + book(nomeLivro).editora );
